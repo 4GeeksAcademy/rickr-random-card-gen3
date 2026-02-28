2dbeea0e76bfaf578927cea541c6499de7454422
Rick Rodriguez <rickrod6502@gmail.com>
2026-02-28 02:00:54 +0000

    random card gen inital comit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  // build the card generator logic
+
+  // Build value and suit arrays
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['♠', '♥', '♣', '♦'];
+
+  // Generate random numbers from array lengths to select a value and suit
+  let randomValue = Math.floor(Math.random() * values.length);
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  
+
+  // Grab the html elements needed to inject suit and value into the DOM
+  let htmlTopSuitElement = document.querySelector('.top-suit');
+  let htmlValueElement = document.querySelector('.value');
+  let htmlBottomSuitElement = document.querySelector('.bottom-suit');
+
+  htmlTopSuitElement.innerHTML = suits[randomSuit];
+  htmlValueElement.innerHTML = values[randomValue];
+  htmlBottomSuitElement.innerHTML = suits[randomSuit];
+
+
+  // generate correct suit color (heart, diamond = red; spade, club = black)
+  if (suits[randomSuit] === '♥' || suits[randomSuit] === '♦') {
+    // add some dynamic red color styling to the heart or diamond
+    htmlTopSuitElement.style.color = 'red';
+    htmlBottomSuitElement.style.color = 'red';
+  }
+
 };
